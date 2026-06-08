df5c0977130676d25f45782f3c888f36a4e65512
Marisel Dev <affulnana112@gmail.com>
2026-06-08 18:02:40 +0000

    Change bot owner and owner number
    Updated bot owner and owner number in settings.

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: 'suprem',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Suprem', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  botOwner: 'Marisel Dev', // Your name
+  ownerNumber: '233241129406', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
